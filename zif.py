2c0859926768829107876cf74f15700742e77f32
Abishek Bhat <bhat.abishek@gmail.com>
2013-03-30 11:48:04 +0530

    Fixing bugs in Zif theorem

--- zif.py
+++ zif.py
@@ -1,9 +1,9 @@
 limit,to_select=map(int,raw_input().split(" "))
 def get_song(index):
-	f1,name=raw_input().split(" ")
+	f,name=raw_input().split(" ")
 	index=index+1
-	return {"index":index,"quality":int(f1),"name":name,"quality_metric":index*int(f1)}
+	return {"index":index,"quality":int(f),"name":name,"quality_metric":index*int(f)}
 songs=[get_song(index) for index in range(limit)]
-sorted_songs=sorted(songs,key=lambda song:song["quality_metric"])
-for song in sorted_songs[::to_select]: print song["name"],song["quality_metric"]
+songs.sort(key=lambda song:(song["quality_metric"],-song["index"]),reverse=True)
+for song in songs[:to_select]: print song["name"]
 
