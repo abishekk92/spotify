5a2e6b8577ca5a46a9b77a7d20188474e07b6409
Abishek <bhat.abishek@gmail.com>
2013-04-02 19:11:36 +0530

    WIP Catvsdog

--- catvsdog.py
+++ catvsdog.py
@@ -1,18 +1,14 @@
 from collections import Counter
 test_cases=int(raw_input())
 def get_vote():
-	keep,kick=raw_input().split(" ")
-	return {"keep":keep,"kick":kick}
+	return tuple(raw_input().split())
 def max_voters():
-	cats,dogs,voters=map(int,raw_input().split(" "))
+	cats,dogs,voters=map(int,raw_input().split())
 	votes=[get_vote() for i in range(voters)]
-	keep=map(lambda vote: vote["keep"],votes)
-	kick=map(lambda vote: vote["kick"],votes)
-	keep_count,kick_count=Counter(keep),Counter(kick)
-	keep_it=[]
-	for item in set(keep_count.keys()+kick_count.keys()):
-		if keep_count[item] >= kick_count[item]:keep_it.append(item)
-	return max(map(lambda pet: keep_count[pet],keep_it))
+	keep_count,kick_count=map(Counter,map(list,zip(*votes)))
+	print "\n".join(map(str,votes))
+	print map(lambda vote: {"degree":keep_count[vote[1]]+kick_count[vote[0]],"vote":vote},votes)
+	return 0
 results=[max_voters() for i in range(test_cases)]
-for result in results : print result
+#for result in results : print result
 
